3a4c37d59c337d3bf38ce835c95d9e8bfebe2c74
Jim Hart <hart0554@umn.edu>
2026-06-24 14:25:28 -0500

    composer update to include laravel/sanctum and correct configs

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,7 +1,5 @@
 <?php
 
-use Laravel\Sanctum\Sanctum;
-
 return [
     /*
     |--------------------------------------------------------------------------
@@ -15,11 +13,17 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,127.0.0.1:3000,'.env('APP_URL'),
-        Sanctum::currentApplicationUrlWithProtocol() === 'http://localhost' ? '' : ','.parse_url(Sanctum::currentApplicationUrlWithProtocol(), PHP_URL_HOST)
-    ))),
+    'stateful' => explode(',', env(
+        'SANCTUM_STATEFUL_DOMAINS',
+        implode(',', array_filter([
+            'localhost',
+            'localhost:3000',
+            '127.0.0.1',
+            '127.0.0.1:8000',
+            '127.0.0.1:3000',
+            parse_url(env('APP_URL', ''), PHP_URL_HOST),
+        ]))
+    )),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,7 +64,7 @@ return [
     */
 
     'middleware' => [
-        'verify_csrf_token' => \App\Http\Middleware\VerifyCsrfToken::class,
+        'verify_csrf_token' => \App\Http\Middleware\PreventRequestForgery::class,
         'encrypt_cookies' => \App\Http\Middleware\EncryptCookies::class,
     ],
 ];
